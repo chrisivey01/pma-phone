8924b2ac2d712f7fee81117399d182b5f0919088
sax <jcmeisner7@gmail.com>
2021-01-14 13:46:29 -0800

    IP UPDATE

--- src/apps/twitter.js
+++ src/apps/twitter.js
@@ -228,7 +228,7 @@ $("#screen-content").on("click", "#photo", (event) => {
     ClosePhone();
     $.post("http://8bit_phone/openCamera",JSON.stringify({
         // Enter box ips HERE w/ PORT & /upload
-        ip: 'http://[IP]/upload',
+        ip: 'http://fivem.pmarp.com:3555/upload',
     }),(resultURL) => {
         if (resultURL != "") {
             let url = resultURL;
